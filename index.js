255ed95faf874060f5a7497441d7c9b31dab3b5e
BinnyGandhi <bkgandhi27296@gmail.com>
2016-09-21 21:25:51 +0530

    Add single digit calculation

--- index.js
+++ index.js
@@ -2,26 +2,59 @@
  * Created by Binny Gandhi on 14-09-2016.
  */
 
+var enterInput;
 var displayInput;
 
 $(document).ready(function () {
-    displayInput = $('#displayInput');
+    enterInput = document.getElementById('enterInput');
+    displayInput = document.getElementById('displayInput');
 });
 
-$(document).keypress(function (e) {
-    if (e.which == 13) {
-        calculateResult();
+function numbers(input) {
+    enterInput.style.borderColor = "#ccc";
+    var currentLastCharacter = displayInput.innerHTML.slice(-1);
+    var currentFirstCharacter = enterInput.innerHTML.charAt(0);
+
+    if ("+-*/%".indexOf(currentLastCharacter) < 0) {
+        displayInput.innerHTML = '';
     }
-});
+    if (input == '.') {
+        fractionNumbers(input);
+    } else {
+        if ("+-*/%".indexOf(currentFirstCharacter) < 0) {
+            enterInput.innerHTML = input.innerHTML;
+        } else {
+            enterInput.innerHTML += input.innerHTML;
+        }
+        displayInput.innerHTML += input.innerHTML;
+    }
+}
 
-function display(input) {
-    displayInput.val(displayInput.val() + input.innerText);
+function fractionNumbers(input) {
+    enterInput.innerHTML += input.innerHTML;
+    displayInput.innerHTML += input.innerHTML;
+}
+
+function operators(input) {
+    displayInput.innerHTML = displayInput.innerHTML + input.innerHTML;
+    enterInput.innerHTML = input.innerHTML;
 }
 
 function clearScreen() {
-    displayInput.val('');
+    enterInput.innerHTML = '';
+    displayInput.innerHTML = '';
 }
 
 function calculateResult() {
-    displayInput.val(eval(displayInput.val()));
+    var result;
+    try {
+        result = parseFloat(eval(displayInput.innerText));
+        enterInput.innerHTML = result;
+        displayInput.innerHTML = result;
+    } catch (err) {
+        displayInput.innerHTML = "Incorrect Input";
+        enterInput.style.borderColor = "#A94442";
+    }
+
+
 }
