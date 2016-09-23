11a6578572a1edfe1104b5b299085a221e3b15a3
BinnyGandhi <bkgandhi27296@gmail.com>
2016-09-23 13:59:04 +0530

    Fix all bugs

--- index.js
+++ index.js
@@ -4,40 +4,64 @@
 
 var enterInput;
 var displayInput;
+var resultBool = false;
+var operatorBool = false;
+var errorBool = false;
 
 $(document).ready(function () {
     enterInput = document.getElementById('enterInput');
     displayInput = document.getElementById('displayInput');
 });
 
-function numbers(input) {
+function simpleFunction(input) {
     enterInput.style.borderColor = "#ccc";
-    var currentLastCharacter = displayInput.innerHTML.slice(-1);
-    var currentFirstCharacter = enterInput.innerHTML.charAt(0);
+    switch (input.innerHTML) {
+        case 'AC':
+            clearScreen();
+            break;
 
-    if ("+-*/%".indexOf(currentLastCharacter) < 0) {
-        displayInput.innerHTML = '';
-    }
-    if (input == '.') {
-        fractionNumbers(input);
-    } else {
-        if ("+-*/%".indexOf(currentFirstCharacter) < 0) {
-            enterInput.innerHTML = input.innerHTML;
-        } else {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '0':
+        case '.':
+            if (resultBool) {
+                enterInput.innerHTML = '';
+                displayInput.innerHTML = '';
+            } else if (operatorBool) {
+                enterInput.innerHTML = '';
+            }
             enterInput.innerHTML += input.innerHTML;
-        }
-        displayInput.innerHTML += input.innerHTML;
+            displayInput.innerHTML += input.innerHTML;
+            resultBool = false;
+            operatorBool = false;
+            break;
+
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '%':
+            if (errorBool) {
+                clearScreen();
+            }
+            errorBool = false;
+            resultBool = false;
+            operatorBool = true;
+            displayInput.innerHTML += input.innerHTML;
+            enterInput.innerHTML = input.innerHTML;
+            break;
+
+        case '=':
+            calculateResult();
+            break;
     }
-}
-
-function fractionNumbers(input) {
-    enterInput.innerHTML += input.innerHTML;
-    displayInput.innerHTML += input.innerHTML;
-}
-
-function operators(input) {
-    displayInput.innerHTML = displayInput.innerHTML + input.innerHTML;
-    enterInput.innerHTML = input.innerHTML;
 }
 
 function clearScreen() {
@@ -49,12 +73,16 @@ function calculateResult() {
     var result;
     try {
         result = parseFloat(eval(displayInput.innerText));
+        if (!isFinite(result) || isNaN(result)) {
+            errorBool = true;
+        }
         enterInput.innerHTML = result;
         displayInput.innerHTML = result;
+        resultBool = true;
     } catch (err) {
         displayInput.innerHTML = "Incorrect Input";
+        enterInput.innerHTML = '';
         enterInput.style.borderColor = "#A94442";
+        errorBool = true;
     }
-
-
 }
